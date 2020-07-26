92799a83621ffa57d900f0c2d150850aa1e9151e
Michael Washington <webmaster@adefwebserver.com>
2020-07-26 16:20:10 -0700

    Blocks Display

---
+++ BlazorSample/Data/Interop.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorSample
+{
+    public static class Interop
+    {
+        internal static ValueTask<object> DemoWorkspace(
+            IJSRuntime jsRuntime,
+            ElementReference blocklyDiv,
+            ElementReference toolbox,
+            ElementReference startBlocks)
+        {
+            return jsRuntime.InvokeAsync<object>(
+                "BlocklyFunctions.createDemoWorkspace",
+                blocklyDiv,
+                toolbox,
+                startBlocks);
+        }
+    }
+}
