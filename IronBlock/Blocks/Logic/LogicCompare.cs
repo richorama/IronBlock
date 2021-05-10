00605e55d0cf824fc71025491d5b940c3e47a280
Richard Astbury <richard.astbury@gmail.com>
2021-05-10 21:39:04 +0100

    updates to logic comparer

--- IronBlock/Blocks/Logic/LogicCompare.cs
+++ IronBlock/Blocks/Logic/LogicCompare.cs
@@ -1,7 +1,7 @@
+using System;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace IronBlock.Blocks.Logic
@@ -10,27 +10,41 @@ namespace IronBlock.Blocks.Logic
   {
     public override object Evaluate(Context context)
     {
-      var a = this.Values.Evaluate("A", context);
-      var b = this.Values.Evaluate("B", context);
+      string opValue = Fields.Get("OP");
 
-      var opValue = this.Fields.Get("OP");
+      object a = Values.Evaluate("A", context);
+      object b = Values.Evaluate("B", context);
 
-      if (a is string) return Compare(opValue, a as string, b as string);
-      if (a is double) return Compare(opValue, (double)a, (double)b);
-      if (a is bool) return Compare(opValue, (bool)a, (bool)b);
+      (bool canConvert, double aValue, double bValue) tryInt = TryConvertToDoubleValues(a, b); // int => blockly always uses double
+      if (tryInt.canConvert)
+        return Compare(opValue, tryInt.aValue, tryInt.bValue);
+
+      (bool canConvert, double aValue, double bValue) tryDouble = TryConvertValues<double>(a, b);
+      if (tryDouble.canConvert)
+        return Compare(opValue, tryDouble.aValue, tryDouble.bValue);
+
+      (bool canConvert, string aValue, string bValue) tryString = TryConvertValues<string>(a, b);
+      if (tryString.canConvert)
+        return Compare(opValue, tryString.aValue, tryString.bValue);
+
+      (bool canConvert, bool aValue, bool bValue) tryBool = TryConvertValues<bool>(a, b);
+      if (tryBool.canConvert)
+        return Compare(opValue, tryBool.aValue, tryBool.bValue);
 
       throw new ApplicationException("unexpected value type");
     }
 
     public override SyntaxNode Generate(Context context)
     {
-      var firstExpression = this.Values.Generate("A", context) as ExpressionSyntax;
-      if (firstExpression == null) throw new ApplicationException($"Unknown expression for value A.");
+      ExpressionSyntax firstExpression = Values.Generate("A", context) as ExpressionSyntax;
+      if (firstExpression == null)
+        throw new ApplicationException("Unknown expression for value A.");
 
-      var secondExpression = this.Values.Generate("B", context) as ExpressionSyntax;
-      if (secondExpression == null) throw new ApplicationException($"Unknown expression for value B.");
+      ExpressionSyntax secondExpression = Values.Generate("B", context) as ExpressionSyntax;
+      if (secondExpression == null)
+        throw new ApplicationException("Unknown expression for value B.");
 
-      var opValue = this.Fields.Get("OP");
+      var opValue = Fields.Get("OP");
 
       var binaryOperator = GetBinaryOperator(opValue);
       var expression = BinaryExpression(binaryOperator, firstExpression, secondExpression);
@@ -38,57 +52,122 @@ namespace IronBlock.Blocks.Logic
       return ParenthesizedExpression(expression);
     }
 
-    bool Compare(string op, string a, string b)
+    private bool Compare(string op, string a, string b)
     {
       switch (op)
       {
-        case "EQ": return a == b;
-        case "NEQ": return a != b;
-        case "LT": return string.Compare(a, b) < 0;
-        case "LTE": return string.Compare(a, b) <= 0;
-        case "GT": return string.Compare(a, b) > 0;
-        case "GTE": return string.Compare(a, b) >= 0;
-        default: throw new ApplicationException($"Unknown OP {op}");
+        case "EQ":
+          return a == b;
+        case "NEQ":
+          return a != b;
+        case "LT":
+          return string.CompareOrdinal(a, b) < 0;
+        case "LTE":
+          return string.CompareOrdinal(a, b) <= 0;
+        case "GT":
+          return string.CompareOrdinal(a, b) > 0;
+        case "GTE":
+          return string.CompareOrdinal(a, b) >= 0;
+        default:
+          throw new ApplicationException($"Unknown OP {op}");
       }
     }
 
-    bool Compare(string op, double a, double b)
+    private bool Compare(string op, double a, double b)
     {
       switch (op)
       {
-        case "EQ": return a == b;
-        case "NEQ": return a != b;
-        case "LT": return a < b;
-        case "LTE": return a <= b;
-        case "GT": return a > b;
-        case "GTE": return a >= b;
-        default: throw new ApplicationException($"Unknown OP {op}");
+        case "EQ":
+          return a.Equals(b);
+        case "NEQ":
+          return !a.Equals(b);
+        case "LT":
+          return a < b;
+        case "LTE":
+          return a <= b;
+        case "GT":
+          return a > b;
+        case "GTE":
+          return a >= b;
+        default:
+          throw new ApplicationException($"Unknown OP {op}");
       }
     }
 
-    bool Compare(string op, bool a, bool b)
+    private bool Compare(string op, bool a, bool b)
     {
       switch (op)
       {
-        case "EQ": return a == b;
-        case "NEQ": return a != b;
-        default: throw new ApplicationException($"Unknown OP {op}");
+        case "EQ":
+          return a == b;
+        case "NEQ":
+          return a != b;
+        case "LT":
+          return Convert.ToByte(a) < Convert.ToByte(b);
+        case "LTE":
+          return Convert.ToByte(a) <= Convert.ToByte(b);
+        case "GT":
+          return Convert.ToByte(a) > Convert.ToByte(b);
+        case "GTE":
+          return Convert.ToByte(a) >= Convert.ToByte(b);
+        default:
+          throw new ApplicationException($"Unknown OP {op}");
       }
     }
 
-    private SyntaxKind GetBinaryOperator(string opValue)
+    private SyntaxKind GetBinaryOperator(string op)
     {
-      switch (opValue)
+      switch (op)
       {
-        case "EQ": return SyntaxKind.EqualsExpression;
-        case "NEQ": return SyntaxKind.NotEqualsExpression;
-        case "LT": return SyntaxKind.LessThanExpression;
-        case "LTE": return SyntaxKind.LessThanOrEqualExpression;
-        case "GT": return SyntaxKind.GreaterThanExpression;
-        case "GTE": return SyntaxKind.GreaterThanOrEqualExpression;
-
-        default: throw new ApplicationException($"Unknown OP {opValue}");
+        case "EQ":
+          return SyntaxKind.EqualsExpression;
+        case "NEQ":
+          return SyntaxKind.NotEqualsExpression;
+        case "LT":
+          return SyntaxKind.LessThanExpression;
+        case "LTE":
+          return SyntaxKind.LessThanOrEqualExpression;
+        case "GT":
+          return SyntaxKind.GreaterThanExpression;
+        case "GTE":
+          return SyntaxKind.GreaterThanOrEqualExpression;
+        default:
+          throw new ApplicationException($"Unknown OP {op}");
       }
+    }
+
+    private (bool canConvert, T aValue, T bValue) TryConvertValues<T>(object a, object b)
+    {
+      T aResult;
+      if (a?.GetType() == typeof(T))
+        aResult = (T)Convert.ChangeType(a, typeof(T));
+      else
+        return (false, default, default);
+
+      T bResult;
+      if (b?.GetType() == typeof(T))
+        bResult = (T)Convert.ChangeType(b, typeof(T));
+      else
+        return (false, default, default);
+
+      return (true, aResult, bResult);
+    }
+
+    private (bool canConvert, double aValue, double bValue) TryConvertToDoubleValues(object a, object b)
+    {
+      double aResult;
+      if (a?.GetType() == typeof(double) || a?.GetType() == typeof(int))
+        aResult = (double)Convert.ChangeType(a, typeof(double));
+      else
+        return (false, default, default);
+
+      double bResult;
+      if (b?.GetType() == typeof(double) || b?.GetType() == typeof(int))
+        bResult = (double)Convert.ChangeType(b, typeof(double));
+      else
+        return (false, default, default);
+
+      return (true, aResult, bResult);
     }
   }
 }
